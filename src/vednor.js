6ea46c161f57328d0b012c685274a83467b3a96f
jbar894 <j.barwellsmith@gmail.com>
2018-04-28 12:21:19 +1200

    Added bundle splitting

---
+++ src/vednor.js
@@ -0,0 +1,8 @@
+/* This file contains reference to the vendor libraries
+we're using in prodution in this project. This is used by webpack
+in the produciton build only*.
+*/
+
+/* eslint-disable no-unused-vars */
+
+import fetch from 'whatwg-fetch';
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -6,16 +6,23 @@ export default {
   debug: true,
   devtool: 'source-map',
   noInfo: false,
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vednor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   plugins: [
+    // Use CommonsChunkPlugin to create a separate bundle
+    // of vendor linraries so that they're cached sperately.
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+
     // Creating HTML file that includes reference to bundled JS
     new HtmlWebpackPlugin({
       template: 'src/index.html',
